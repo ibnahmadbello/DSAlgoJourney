0ea08181a64da813bebf39e82b20c29e6be7f088
ibnahmadbello <justforlearningcode@gmail.com>
2021-12-26 18:41:22 +0100

    Fibonacci sequence using Memoization with Array structure

--- src/FibSeqWithMemoization.java
+++ src/FibSeqWithMemoization.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+import java.util.Map;
 
 public class FibSeqWithMemoization {
 
@@ -6,13 +8,46 @@ public class FibSeqWithMemoization {
 		System.out.println(fib(6));
 		System.out.println(fib(7));
 		System.out.println(fib(8));
+		System.out.println(fib(46));
+		System.out.println(fib(47));
 		System.out.println(fib(50));
+		System.out.println(fib(100));
+		
+		System.out.println();
+		
+//		System.out.println(fibWithMap(6));
+//		System.out.println(fibWithMap(7));
+//		System.out.println(fibWithMap(8));
+//		System.out.println(fibWithMap(46));
+//		System.out.println(fibWithMap(47));
+//		System.out.println(fibWithMap(50));
+
 	}
-	
-	public static int fib(int n) {
-		if(n <= 2)
-			return 1;
-		return fib(n - 1) + fib(n - 2);
+
+	public static long fib(int n) {
+		long fibArray[] = new long[(int)(n + 2)];
+		fibArray[0] = 0;
+		fibArray[1] = 1;
+		for (int i = 2; i <= n; i++) {
+			fibArray[i] = fibArray[i -1] + fibArray[i - 2];			
+		}
+		
+		return fibArray[n];
 	}
+
+//	public static long fibWithMap(long i) {
+//		Map<Long, Long> fibMap = new HashMap<Long, Long>();
+//		if(i == 0 || i == 1) {
+//			return i;
+//		}
+//		if (fibMap.containsKey(i)) {
+//				return fibMap.get(i);
+//			}
+//		
+//		long fibForN = fibWithMap(i - 1) + fibWithMap(i - 2);
+//		fibMap.put(i, fibForN);
+//		
+//		return fibForN;
+//	}
 
 }
