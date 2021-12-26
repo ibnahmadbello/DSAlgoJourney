01535323b62f9727d03d51321fb642e90f38a4a1
ibnahmadbello <justforlearningcode@gmail.com>
2021-12-26 15:03:59 +0100

    Fibonacci sequence generated with recursion

--- src/FibonacciSequence.java
+++ src/FibonacciSequence.java
@@ -3,15 +3,16 @@ public class FibonacciSequence {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(fib(5));
+		System.out.println(fib(6));
+		System.out.println(fib(7));
 		System.out.println(fib(8));
-		
+		System.out.println(fib(50));
 	}
 	
 	public static int fib(int n) {
 		if(n <= 2)
 			return 1;
-		return fib(n -1) + (n - 2);
+		return fib(n - 1) + fib(n - 2);
 	}
 
 }
