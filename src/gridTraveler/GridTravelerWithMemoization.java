4f5b966b30f6f0af872798b55d0e110807c4333a
ibnahmadbello <justforlearningcode@gmail.com>
2021-12-31 23:15:09 +0100

    Grid Traveler optimized with memoization

--- src/gridTraveler/GridTravelerWithMemoization.java
+++ src/gridTraveler/GridTravelerWithMemoization.java
@@ -1,10 +1,32 @@
 package gridTraveler;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 public class GridTravelerWithMemoization {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+//		System.out.println(numberOfRoute(1, 1));
+//		System.out.println(numberOfRoute(2, 3));
+//		System.out.println(numberOfRoute(3, 2));
+//		System.out.println(numberOfRoute(3, 3));
+//		System.out.println(numberOfRoute(10, 9));
+		System.out.println(numberOfRoute(18, 18));
+	}
+	
+	public static int numberOfRoute(int row, int column, HashMap<String, Integer> routeMap) {
+		String key = row + "," + column;
+		if(routeMap.containsKey(key)) return routeMap.get(key);
+		if(row == 1 && column == 1) return 1;
+		if(row == 0 || column == 0) return 0;
+		routeMap.put(key, numberOfRoute(row - 1, column, routeMap) + numberOfRoute(row, column - 1, routeMap));
+		return routeMap.get(key);
+	}
+	
+	public static Integer numberOfRoute(int row, int column) {
+		return numberOfRoute(row, column, new HashMap<>());
 	}
 
 }
