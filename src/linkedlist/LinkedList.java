5476a0445736a9ef18598e85d4ac13535d8cc4a4
ibnahmadbello <justforlearningcode@gmail.com>
2022-01-22 21:49:35 +0100

    Implemented linkedlist class and Added class for sum of linkedlist

--- src/linkedlist/LinkedList.java
+++ src/linkedlist/LinkedList.java
@@ -2,4 +2,13 @@ package linkedlist;
 
 public class LinkedList {
 
+	public int value;
+	public LinkedList next;
+	
+	public LinkedList(int value) {
+		this.value = value;
+		this.next = null;
+	}
+	
+	
 }
---
+++ src/linkedlist/SumOfLinkedList.java
@@ -0,0 +1,16 @@
+package linkedlist;
+
+public class SumOfLinkedList {
+
+	public LinkedList sumOfLinkedLists(LinkedList linkedListOne, LinkedList linkedListTwo) {
+		LinkedList nextHeadPointer = new LinkedList(0);
+		LinkedList currentNode = nextHeadPointer;
+		int carry = 0;
+		
+		LinkedList nodeOne = linkedListOne;
+		LinkedList nodeTwo = linkedListTwo;
+		
+		
+		return null;
+	}
+}
