6af2dda23880b7e68b7b9efcf865d7e64b666c58
ibnahmadbello <justforlearningcode@gmail.com>
2022-09-25 09:15:34 +0100

    Re-arrange array element based on the index of the member element using map

--- src/leetcodePractice/ArrangeArray.java
+++ src/leetcodePractice/ArrangeArray.java
@@ -1,11 +1,14 @@
 package leetcodePractice;
 
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class ArrangeArray {
 
 	public static void main(String[] args) {
 		System.out.println(Arrays.toString(arrangeArray(new int[]{5, 3, 2, 4, 0, 1})));
+		System.out.println(arrangeArraywithMap(new int[]{5, 3, 2, 4, 0, 1}));
+
 	}
 	
 	private static int [] arrangeArray(int[] nums) {
@@ -14,6 +17,14 @@ public class ArrangeArray {
 			ans[i] = nums[nums[i]];
 		}
 		return ans;
+	}
+	
+	private static HashMap<Integer,Integer> arrangeArraywithMap(int[] nums) {
+		HashMap<Integer, Integer> arrangeMap = new HashMap<>();
+		for (int i = 0; i < nums.length; i++) {
+			arrangeMap.put(i, nums[nums[i]]);
+		}
+		return arrangeMap;
 	}
 
 }
